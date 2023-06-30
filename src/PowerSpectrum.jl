a68b6f07a89f7a63c4e5e988f13ee17ce32c3917
Gaétan Facchinetti <gaetanfacc@gmail.com>
2023-06-30 17:00:35 +0200

    implementing sigma^2 and better power_spectrum functions

--- src/PowerSpectrum.jl
+++ src/PowerSpectrum.jl
@@ -4,59 +4,54 @@ include("./MyCosmology.jl")
 include("./TransferFunction.jl")
 
 
-import Main.Cosmojuly.MyCosmology: Cosmology, growth_factor, growth_factor_Carroll, hubble_constant, planck18
-import Main.Cosmojuly.TransferFunction: transfer_function, ParametersTF, parametersEH98_planck18
+import Main.Cosmojuly.MyCosmology: Cosmology, growth_factor, growth_factor_Carroll, hubble_constant, planck18, ρ_m_Msun_Mpc3
+import Main.Cosmojuly.TransferFunction: transfer_function, TransferFunctionModel, EH98_planck18
 import Unitful: NoUnits, km, s
 import UnitfulAstro: Mpc, Gpc, Msun
 import PhysicalConstants.CODATA2018: c_0
 
-
-using QuadGK
+import QuadGK: quadgk
 
 export curvature_power_spectrum, matter_power_spectrum
-export window_function, Window, Real_space_top_hat, Sharp_k, Exponential
+export window_function, Window, TopHat, SharpK, Exponential
+export radius_from_mass, mass_from_radius, σ²_m, σ²_χ
 
 """ ΛCDM power-law power spectrum (dimensionless) at k_Mpc (in 1/Mpc) """
 function power_spectrum_ΛCDM(k_Mpc::Real, amplitude::Real = 1e-10*exp(3.044), index::Real = 0.9649)
     return amplitude * (k_Mpc / 0.05)^(index-1)
 end
 
-# TO DO
-#""" ΛCDM power-law power spectrum (dimensionless) at k_Mpc (in 1/Mpc) with an extra spike inside"""
-#function power_spectrum_with_spike(k_Mpc::Real, amplitude_1::Real = 1e-10*exp(3.044), index::Real = 0.9649, amplitude_star::Real = 0.0, k_star::Real = )
-
 """ Curvature power spectrum (in Mpc^3) at k_Mpc (in 1/Mpc) """
-function curvature_power_spectrum(k_Mpc::Real, power_spectrum::Function = power_spectrum_ΛCDM, params...)::Real 
-    return 2.0 * pi^2 * power_spectrum(k_Mpc, params...) / k_Mpc^3
+function curvature_power_spectrum(k_Mpc::Real, power_spectrum::Function = power_spectrum_ΛCDM)
+    return 2.0 * pi^2 * power_spectrum(k_Mpc) / k_Mpc^3
 end 
 
 """ Matter power spectrum (in Mpc^3) at k_Mpc (in 1/Mpc) """
-function matter_power_spectrum(k_Mpc::Real, z::Real = 0, 
-    power_spectrum::Function    = power_spectrum_ΛCDM, 
-    transfer_function::Function = transfer_function, 
+function matter_power_spectrum( 
+    k_Mpc::Real, 
+    z::Real = 0.0;
+    power_spectrum::Function = power_spectrum_ΛCDM, 
     cosmology::Cosmology{<:Real} = planck18,
-    params_ps... = (1e-10*exp(3.044), 0.9649)...;
+    transferFunctionModel::Union{Nothing, <:TransferFunctionModel} = nothing,
+    dimensionless = false,
     approximate_growth_factor::Bool = true,
     with_baryons::Bool = true)
 
     _c_over_H0_Mpc = c_0 / (hubble_constant(cosmology) * km / s / Mpc) / Mpc |> NoUnits 
     _D1_z = approximate_growth_factor ?  growth_factor_Carroll(z, cosmology) : growth_factor(z, cosmology)
 
-    pref = (4. / 25.) * (_D1_z * k_Mpc^2 * transfer_function(k_Mpc, cosmology, with_baryons = with_baryons) * _c_over_H0_Mpc^2 / cosmology.Ω_m0)^2
-    return pref * curvature_power_spectrum(k_Mpc, power_spectrum, params_ps...) 
+    ## Accelerate computation with planck18 cosmology
+    ## This can be done for other pre-implemented cosmologies
+    if cosmology === planck18
+        transferFunctionModel = isnothing(transferFunctionModel) ? EH98_planck18 : transferFunctionModel
+    end
+
+    tf = isnothing(transferFunctionModel) ? transfer_function(k_Mpc, cosmology, with_baryons = with_baryons) : transfer_function(k_Mpc, transferFunctionModel, with_baryons = with_baryons)
+
+    prefactor = !dimensionless ? 1 : k_Mpc^3/(2*π^2)
+
+    return prefactor * (4. / 25.) * (_D1_z * k_Mpc^2 * tf * _c_over_H0_Mpc^2 / cosmology.Ω_m0)^2 * curvature_power_spectrum(k_Mpc, power_spectrum) 
 end
-
-
-""" Matter power spectrum (in Mpc^3) at k_Mpc (in 1/Mpc) optimized for planck18 ΛCDM """
-function matter_power_spectrum(k_Mpc::Real, z::Real = 0; approximate_growth_factor::Bool = true, with_baryons::Bool = true)
-
-    _c_over_H0_Mpc = c_0 / (hubble_constant(planck18) * km / s / Mpc) / Mpc |> NoUnits 
-    _D1_z = approximate_growth_factor ?  growth_factor_Carroll(z, planck18) : growth_factor(z, planck18)
-
-    pref = (4. / 25.) * (_D1_z * k_Mpc^2 * transfer_function(k_Mpc, parametersEH98_planck18, with_baryons = with_baryons) * _c_over_H0_Mpc^2 / planck18.Ω_m0)^2
-    return pref * curvature_power_spectrum(k_Mpc, power_spectrum_ΛCDM, 1e-10*exp(3.044), 0.9649) 
-end
-
 
 
 
@@ -69,42 +64,72 @@ abstract type TopHat <: Window end
 abstract type SharpK <: Window end
 abstract type Exponential <: Window end
 
-window_function(kR::Real, ::Type{TopHat})::Real     = kR > 1e-3 ? 3.0 * (sin(kR) - kR * cos(kR)) / kR^3 : 1.0 - kR^2/10.0 
-window_function(kR::Real, ::Type{SharpK})::Real     = 1 - kR > 0 ? 1.0 : 0.0
-window_function(kR::Real, ::Type{Exponential})::Real = exp(-kR * kR / 2.0)
+window_function(kR::Real, ::Type{TopHat})      = kR > 1e-3 ? 3.0 * (sin(kR) - kR * cos(kR)) / kR^3 : 1.0 - kR^2/10.0 
+window_function(kR::Real, ::Type{SharpK})      = 1 - kR > 0 ? 1.0 : 0.0
+window_function(kR::Real, ::Type{Exponential}) = exp(-kR * kR / 2.0)
 
-volume_factor(::Type{TopHat})::Real = 4.0/3.0 * π 
-volume_factor(::Type{SharpK})::Real = 6.0 * π^2 
-volume_factor(::Type{Exponential})::Real = (2*π)^(3//2)
+volume_factor(::Type{TopHat})      = 4.0/3.0 * π 
+volume_factor(::Type{SharpK})      = 6.0 * π^2 
+volume_factor(::Type{Exponential}) = (2*π)^(3//2)
+
 
 """ 
-    mass_vs_lagrangian_radius(R_Mpc, [volume factor, cosmo]) in Msun
+    mass_from_lagrangian_radius(R_Mpc, [volume factor, cosmo]) in Msun
 
 # Arguments
 - `R_Mpc`: radius in Mpc
 - `volume_factor`: volume_factor to relate mass and radius
 - `cosmo` : cosmology 
 """
-mass_vs_radius(R_Mpc::Real; volume_factor::Real=6*π^2, cosmo::MyCosmology.Cosmology = planck18)::Real = volume_factor * cosmo.ρ_c0_Msun_Mpc3 * R_Mpc^3
+mass_from_radius(R_Mpc::Real, ::Type{T} = TopHat; cosmo::Cosmology = planck18) where {T<:Window}  = volume_factor(T) * ρ_m_Msun_Mpc3(0, cosmo) * R_Mpc^3
 
 
 """ 
-    radius_vs_mass(M_Msun, [volume factor, cosmo]) in Msun
+    radius_from_mass(M_Msun, [volume factor, cosmo]) in Msun
 
 # Arguments
 - `M_Msun`: radius in Msun
 - `volume_factor`: volume_factor to relate mass and radius
 - `cosmo` : cosmology 
 """
-radius_vs_mass(M_Msun::Real; volume_factor::Real=6*π^2, cosmo::MyCosmology.Cosmology = planck18)::Real = (volume_factor * cosmo.ρ_c0_Msun_Mpc3 * M_Msun)^(1/3)
+radius_from_mass(M_Msun::Real, ::Type{T} = TopHat; cosmo::Cosmology = planck18) where {T<:Window} = (M_Msun / volume_factor(T) / ρ_m_Msun_Mpc3(0, cosmo))^(1/3)
 
 
-radius_vs_mass(M_Msun::Real, ::Type{T} = TopHat; cosmo::MyCosmology.Cosmology = planck18)::Real where {T<:Window} = (volume_factor(T) * cosmo.ρ_c0_Msun_Mpc3 * M_Msun)^(1/3)
-mass_vs_radius(R_Mpc::Real, ::Type{T} = TopHat; cosmo::MyCosmology.Cosmology = planck18)::Real where {T<:Window}  = volume_factor(T) * cosmo.ρ_c0_Msun_Mpc3 * R_Mpc^3
+function σ²(R_Mpc::Real, ::Type{TopHat}, matter_ps::Function; kws...)
+    return log(20.0) - log(R_Mpc) > -10.0 ? quadgk(lnk -> matter_ps(exp(lnk)) * window_function(exp(lnk) * R_Mpc, TopHat)^2, -10.0, log(20.0) - log(R_Mpc), rtol=1e-3; kws...)[1] : 0.0
+end
 
-#function sigma2(R_Mpc::Real, ::Type{TopHat}; matter_power_spectrum::Function = matter_power_spectrum) 
-#    return QuadGK.quadgk(matter_power_spectrum)
-#end
+function σ²(R_Mpc::Real, ::Type{SharpK}, matter_ps::Function; kws...)
+    return - log(R_Mpc) > -10.0 ? quadgk(lnk -> matter_ps(exp(lnk)), -10.0, -log(R_Mpc), rtol=1e-3; kws...)[1] : 0.0
+end
+
+
+function σ²_m(
+    R_Mpc::Real, 
+    ::Type{T} = TopHat, 
+    power_spectrum::Function = power_spectrum_ΛCDM,  
+    cosmology::Cosmology{<:Real} = planck18,
+    transferFunctionModel::Union{Nothing, TransferFunctionModel} = nothing,
+    kws...) where {T<:Window}
+
+    return σ²(R_Mpc, T, k->matter_power_spectrum(k, 0; power_spectrum = power_spectrum, cosmology = cosmology, transferFunctionModel = transferFunctionModel, dimensionless=true); kws...)
+end
+
+function σ²_χ(
+    R_Mpc::Real, 
+    ::Type{T} = TopHat, 
+    power_spectrum::Function = power_spectrum_ΛCDM,  
+    cosmology::Cosmology{<:Real} = planck18,
+    transferFunctionModel::Union{Nothing, TransferFunctionModel} = nothing,
+    kws...) where {T<:Window}
+
+    return σ²(R_Mpc, T, k->matter_power_spectrum(k, 0; power_spectrum = power_spectrum, cosmology = cosmology, transferFunctionModel = transferFunctionModel, dimensionless=true, with_baryons=false); kws...)
+end
+
+
+
+
+
 
 
 end # module PowerSpectrum
--- src/TransferFunction.jl
+++ src/TransferFunction.jl
@@ -5,11 +5,11 @@ include("./MyCosmology.jl")
 
 import Main.Cosmojuly.MyCosmology: Cosmology, z_eq_mr, k_eq_mr_Mpc, planck18
 
-export transfer_function
+export transfer_function, TrivialTF, EH98, TransferFunctionModel
 
-abstract type ParametersTF{T<:Real} end
+abstract type TransferFunctionModel end
 
-struct ParametersEH98{T<:Real} <: ParametersTF{T}
+struct EH98{T<:Real} <: TransferFunctionModel
     
     # Parameters directly related to the Cosmology
     Ω_m0_h2::T
@@ -25,13 +25,13 @@ struct ParametersEH98{T<:Real} <: ParametersTF{T}
     β_c::T
     β_b::T
     k_Silk_Mpc::T
-   
+
 end
 
 g_func(y::Real)::Real = -6.0*sqrt(1.0+y)+(2.0+3.0*y)*log((sqrt(1.0+y)+1)/(sqrt(1.0+y)-1.0)) * y
 
 
-function ParametersEH98(Ω_m0_h2::Real, Ω_b0_h2::Real, Ω_χ0_h2::Real, z_eq_mr::Real, k_eq_mr_Mpc::Real, ::Type{T}; T0_CMB_K::Real = 2.72548) where {T<:Real}
+function EH98(Ω_m0_h2::Real, Ω_b0_h2::Real, Ω_χ0_h2::Real, z_eq_mr::Real, k_eq_mr_Mpc::Real, ::Type{T}; T0_CMB_K::Real = 2.72548) where {T<:Real}
     
     Θ27::T = T0_CMB_K / 2.7
 
@@ -60,10 +60,10 @@ function ParametersEH98(Ω_m0_h2::Real, Ω_b0_h2::Real, Ω_χ0_h2::Real, z_eq_mr
     α_b::T = 2.07 * k_eq_mr_Mpc * sound_horizon_Mpc * (1.0 + R_drag)^(-3.0/4.0) * g_func( (1.0 + z_eq_mr) / (1.0 + z_drag) )
     β_b::T = 0.5 + Ω_b0_h2 / Ω_m0_h2 + (3.0 - 2.0 * Ω_b0_h2/Ω_m0_h2) * sqrt( 1.0 + (17.2 * Ω_m0_h2)^2 )
 
-    ParametersEH98(convert(T, Ω_m0_h2), convert(T, Ω_b0_h2), convert(T, Ω_χ0_h2), convert(T, Θ27), z_drag, sound_horizon_Mpc, α_c, α_b, β_c, β_b, k_Silk_Mpc)
+    EH98(convert(T, Ω_m0_h2), convert(T, Ω_b0_h2), convert(T, Ω_χ0_h2), convert(T, Θ27), z_drag, sound_horizon_Mpc, α_c, α_b, β_c, β_b, k_Silk_Mpc)
 end
 
-function ParametersEH98(cosmo::Cosmology{<:Real}, ::Type{T} = Float64) where {T<:Real}
+function EH98(cosmo::Cosmology{<:Real}, ::Type{T} = Float64) where {T<:Real}
 
     Ω_m0_h2::T = cosmo.Ω_m0 * cosmo.h^2
     Ω_b0_h2::T = cosmo.Ω_b0 * cosmo.h^2
@@ -72,10 +72,10 @@ function ParametersEH98(cosmo::Cosmology{<:Real}, ::Type{T} = Float64) where {T<
     z_eq = z_eq_mr(cosmo)
     k_eq_Mpc = k_eq_mr_Mpc(cosmo)
 
-    return ParametersEH98(Ω_m0_h2, Ω_b0_h2, Ω_χ0_h2, z_eq, k_eq_Mpc, T, T0_CMB_K = cosmo.T0_CMB_K)
+    return EH98(Ω_m0_h2, Ω_b0_h2, Ω_χ0_h2, z_eq, k_eq_Mpc, T, T0_CMB_K = cosmo.T0_CMB_K)
 end
 
-const parametersEH98_planck18 = ParametersEH98(planck18)
+const EH98_planck18 = EH98(planck18)
 
 function transfer_0_tilde(q::Real, α_c::Real, β_c::Real)::Real
     C = 14.2 / α_c + 386.0 / (1.0 + 69.9 * q^1.08 )
@@ -83,39 +83,50 @@ function transfer_0_tilde(q::Real, α_c::Real, β_c::Real)::Real
 end 
 
 shape_parameter(k_Mpc::Real, T0_CMB_K::Real, Ω_m0_h2::Real)=  k_Mpc * (T0_CMB_K/2.7)^2 / Ω_m0_h2
-shape_parameter(k_Mpc::Real, p::ParametersEH98)::Real = shape_parameter(k_Mpc, 2.7 * p.Θ27, p.Ω_m0_h2)
+shape_parameter(k_Mpc::Real, p::EH98)::Real = shape_parameter(k_Mpc, 2.7 * p.Θ27, p.Ω_m0_h2)
 
-function transfer_cdm(k_Mpc::Real, p::ParametersEH98)::Real
+function transfer_cdm(k_Mpc::Real, p::EH98)::Real
     q = shape_parameter(k_Mpc, p)
     f = 1.0 / (1.0  +  (k_Mpc * p.sound_horizon_Mpc / 5.4)^4)
     return f * transfer_0_tilde(q, 1.0, p.β_c) + (1.0 - f) * transfer_0_tilde(q, p.α_c, p.β_c)
 end
 
-function s_tilde_Mpc(k_Mpc::Real, p::ParametersEH98)::Real
+function s_tilde_Mpc(k_Mpc::Real, p::EH98)::Real
     β_node = 8.41 * (p.Ω_m0_h2)^0.435
     return p.sound_horizon_Mpc * (1.0 + (β_node/(k_Mpc * p.sound_horizon_Mpc))^3 )^(-1.0/3.0)
 end
 
-function transfer_baryons(k_Mpc::Real, p::ParametersEH98)::Real
+function transfer_baryons(k_Mpc::Real, p::EH98)::Real
     q = shape_parameter(k_Mpc, p)
     j0 = sinc(k_Mpc * s_tilde_Mpc(k_Mpc, p) / π)
     ks = k_Mpc * p.sound_horizon_Mpc
     return j0 * (transfer_0_tilde(q, 1.0, 1.0) / (1.0 + (ks /5.2)^2 ) + p.α_b * exp(-(k_Mpc/p.k_Silk_Mpc)^1.4) / (1.0 + (p.β_b/ks)^3 ) )
 end
 
-function transfer_function(k_Mpc::Real, p::ParametersEH98 = parametersEH98_planck18; with_baryons::Bool = true)::Real
-    tc = transfer_cdm(k_Mpc, p)
-    tb = transfer_baryons(k_Mpc, p)
-    return with_baryons ? p.Ω_b0_h2 / p.Ω_m0_h2 * tb + p.Ω_χ0_h2/p.Ω_m0_h2 * tc : p.Ω_χ0_h2 / p.Ω_m0_h2 * tc
-end
 
+
+
+"""
+    transfer_function
+"""
 function transfer_function(k_Mpc::Real, cosmo::Cosmology{<:Real} = planck18; with_baryons::Bool = true)
-    p = ParametersEH98(cosmo)
+    p = EH98(cosmo)
     tc = transfer_cdm(k_Mpc, p)
     tb = transfer_baryons(k_Mpc, p)
     return with_baryons ? p.Ω_b0_h2 / p.Ω_m0_h2 * tb + p.Ω_χ0_h2/p.Ω_m0_h2 * tc : p.Ω_χ0_h2 / p.Ω_m0_h2 * tc
 end
 
-function transfer_function(k_Mpc::Real, p::ParametersTF{<:Real}; with_baryons::Bool = true)::Real end
+
+function transfer_function(k_Mpc::Real, p::EH98 = EH98_planck18; with_baryons::Bool = true)
+    tc = transfer_cdm(k_Mpc, p)
+    tb = transfer_baryons(k_Mpc, p)
+    return with_baryons ? p.Ω_b0_h2 / p.Ω_m0_h2 * tb + p.Ω_χ0_h2/p.Ω_m0_h2 * tc : p.Ω_χ0_h2 / p.Ω_m0_h2 * tc
+end
+
+## Define here the trivial transfer function
+struct TrivialTF <: TransferFunctionModel end
+transfer_function(k_Mpc::Real, p::TrivialTF; with_baryons::Bool = true) = 1.0
+
+## Other transfer functions can be implemented down here
 
 end # module TransferFunction
