ec85a4b1a5f74f7685f8612a0a53057d97242c23
Gaétan Facchinetti <gaetanfacc@gmail.com>
2023-09-18 15:52:10 +0200

    starting to implement galactic quantities

--- src/FSL.jl
+++ src/FSL.jl
@@ -3,6 +3,9 @@ module FSLModel
 
 #include("./Halos.jl")
 
+import Main.Cosmojuly.BackgroundCosmo: BkgCosmology, planck18_bkg
+import Main.Cosmojuly.PowerSpectrum: Cosmology, planck18
+
 export subhalo_mass_function_template
 export mass_function_merger_tree
 
@@ -41,27 +44,68 @@ function mass_function_merger_tree(mΔ_sub::Real, mΔ_host::Real)
 end
 
 
-function pdf_concentration(cΔ::Real, mΔ::Real) ::Real
+function pdf_concentration(cΔ::Real, mΔ::Real; median::Function = median_concentration_SCP12, std::Function = m -> 0.14 * log(10.0))
    
-    # Definition of properties from Sanchez-Conde & Prada relation
-    σ_c = 0.14 * log(10.0)
-    median_c = c_bar(mΔ)
+    σ_c = std(mΔ)
+    median_c = median(mΔ)
 
     Kc = 0.5 * erfc(-log(median_c) / (sqrt(2.0) * σ_c))
 
-    return 1.0 / Kc / c200 / sqrt(2.0 * π) / σ_c * exp(-(log(cΔ) - log(median_c)) / sqrt(2.0) / σ_c^2)
-
+    return 1.0 / Kc / cΔ / sqrt(2.0 * π) / σ_c * exp(-(log(cΔ) - log(median_c)) / sqrt(2.0) / σ_c^2)
 end
 
-#m200 in Msol
-function median_concentration_SCP12(mΔ::Real, h::Real)::Real
 
+#m200 in Msol
+function median_concentration_SCP12(mΔ::Real, h::Real)
     cn::Vector = [37.5153, -1.5093, 1.636e-2, 3.66e-4, -2.89237e-5, 5.32e-7]
     mΔ_min = (mΔ > 7.24e-10) ? mΔ : 7.24e-10
     return sum(cn .* log(mΔ_min * h).^ (0:5))
 end
 
+median_concentration_SCP12(mΔ::Real, bkg_cosmo::BkgCosmology = planck18_bkg) = median_concentration_SCP12(mΔ, bkg_cosmo.h)
+median_concentration_SCP12(mΔ::Real, cosmo::Cosmology = planck18) = median_concentration_SCP12(mΔ, cosmo.bkg.h)
+
+
 #############################################################
+
+# Model of galaxies
+
+struct Bulge{T<:Real}
+    ρ0b::T
+    r0::T
+    q::T
+    rcut::T
+end
+
+abstract type Disc{T<:Real} end
+
+struct StellarDisc{T<:Real} <: Disc{T}
+    Σ0::T
+    zd::T
+    Rd::T
+end
+
+# Definition of the profiles
+density_spherical_BG02() = 0
+density_exponential_disc(R::Real, z::Real; Σ0::Real, Rd::Real, zd::Real) = Σ0/(2.0*zd)*exp(-abs(z)/zd - R/Rd)
+density_sech_disc(R::Real, z::Real; Σ0::Real, Rd::Real, Rm::Real, zd::Real)  = Σ0/(4.0*zd)*exp(-Rm/R - R/Rd)*(sech(z/(2.0*zd)))^2
+
+density_exponential_disc(R::Real, z::Real; stellar_disc::StellarDisc) = density_exponential_disc(R, z, Σ0 = stellar_disc.Σ0, Rd = stellar_disc.Rd, zd = stellar_disc.zd)
+
+struct GasDisc{T<:Real} <: Disc{T}
+    Σ0::T
+    zd::T
+    Rm::T
+    Td::T
+end
+
+struct DiscGalaxy
+    halo::Halo
+    thick_stellar_disk::StellarDisk
+    thin_stellar_disk::StellarDisk
+end
+
+
 
 
 end # module FSL
